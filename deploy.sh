0ce2f599d7609fa3f1c1533b66e580394a358c99
ethicsbowlchina <3208100056@qq.com>
2020-05-10 13:33:53 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:ethicsbowlchina-login/ethicsbowlchina-login.github.io.git master
+git push -f git@github.com:ethicsbowlchina-login/ethicsbowlchina-login.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
-
